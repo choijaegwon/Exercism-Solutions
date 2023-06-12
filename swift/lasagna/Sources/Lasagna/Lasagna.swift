b583d56df51ec7af74c7e60139a2001dd5a9fcd2
choijaegwon <chl9338@naver.com>
2023-06-12 23:05:18 +0900

    lasagna

---
+++ swift/lasagna/Sources/Lasagna/Lasagna.swift
@@ -0,0 +1,13 @@
+var expectedMinutesInOven: Int = 40
+
+func remainingMinutesInOven(elapsedMinutes: Int) -> Int {
+    return expectedMinutesInOven - elapsedMinutes
+}
+
+func preparationTimeInMinutes(layers: Int) -> Int {
+    return 2*layers
+}
+
+func totalTimeInMinutes(layers: Int, elapsedMinutes: Int) -> Int {
+    return preparationTimeInMinutes(layers: layers) + elapsedMinutes
+}
